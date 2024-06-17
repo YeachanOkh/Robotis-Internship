5173dedbba4ac57948d3f45477446fe0b442e09b
Botbuild <andrewyu1470963@gmail.com>
2024-06-17 15:01:02 -0700

    Motor Test

--- MotorControl/Integration.py
+++ MotorControl/Integration.py
@@ -306,7 +306,7 @@ if __name__ == "__main__":
     # motor.simMotorRun([30, 222, 260, 47, 272], [0, 1, 2, 3, 4])  # Reset claw looking up
     # time.sleep(2)
     motor.dxlSetVelo([30, 30, 30, 30, 30], [0, 1, 2, 3, 4])  # ALWAYS SET SPEED BEFORE ANYTHING
-    motor.simMotorRun([100], [1])
+    motor.simMotorRun([500], [1])
     motor.simMotorRun([45], [2])
     motor.simMotorRun([265], [3])  
     motor.simMotorRun([180], [4])# Reset claw looking up
