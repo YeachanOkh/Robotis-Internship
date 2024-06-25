ee2cd07a10d2ad23a66c9a757cbdacefa8684c3e
YeachanOkh <yeachanokh@gmail.com>
2024-06-24 17:34:20 -0700

    hand detection

--- MotorControl/Integration.py
+++ MotorControl/Integration.py
@@ -155,26 +155,27 @@ def debug_gcs_push_in():
     motor.simMotorRun([98, 225, 260, 47, 272], [0, 1, 2, 3, 4])  # Reset claw looking up
     time.sleep(1)
 
-# def Hello():
-#     start_time = time.time()
-# if __name__ == "__main__":
-#     print("set up move")
-#     motor.dxlSetVelo([30, 55, 30, 30, 30], [0, 1, 2, 3, 4])
-#     motor.simMotorRun([90,270,140,265,180], [0,1,2,3,4])
-#     time.sleep(3)
-#     motor.simMotorRun([180,50,265], [3,2,4])
-#     time.sleep(0.3)
-#     motor.simMotorRun([240],[1])
-#     time.sleep(0.3)
-#     motor.simMotorRun([300],[1])
-#     time.sleep(0.3)
-#     motor.simMotorRun([240],[1])
-#     time.sleep(0.3)
-#     motor.simMotorRun([300],[1])
+def Hello():
+    start_time = time.time()
+if __name__ == "__main__":
+    print("set up move")
+    motor.dxlSetVelo([30, 55, 30, 30, 30], [0, 1, 2, 3, 4])
+    motor.simMotorRun([90,270,140,265,180], [0,1,2,3,4])
+    time.sleep(2)
+    motor.simMotorRun([180,50,265], [3,2,4])
+    time.sleep(0.1)
+    motor.simMotorRun([240],[1])
+    time.sleep(0.1)
+    motor.simMotorRun([300],[1])
+    time.sleep(0.1)
+    motor.simMotorRun([240],[1])
+    time.sleep(0.1)
+    motor.simMotorRun([300],[1])
 
 def Thankyou():
     start_time = time.time()
-if __name__ == "__main__":
+def yesno():
+    start_time=time.time()
     print("set up move")
     motor.dxlSetVelo([30, 30, 30, 30, 55], [0, 1, 2, 3, 4])
     motor.simMotorRun([90,270,140,265,180], [0,1,2,3,4])
@@ -188,6 +189,7 @@ if __name__ == "__main__":
     motor.simMotorRun([180],[4])
     time.sleep(0.2)
     motor.simMotorRun([265],[4])
+if __name__ == "__main__":
 
 
     
@@ -219,4 +221,3 @@ if __name__ == "__main__":
     # debug_bvm_pull_out()
     # time.sleep(5)
     # debug_bvm_push_in()
-    
