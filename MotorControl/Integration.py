3f3525120c828f7d791f4efb1387de05c259816f
Botbuild <andrewyu1470963@gmail.com>
2024-06-26 14:28:28 -0700

    Motor Test

--- MotorControl/Integration.py
+++ MotorControl/Integration.py
@@ -229,7 +229,9 @@ def Highfive():
 if __name__ == "__main__":
     print("set up move")
     motor.dxlSetVelo([30, 30, 30, 30, 65], [0, 1, 2, 3, 4])
-    motor.simMotorRun([180,270,140,265,180], [0,1,2,3,4])
+    motor.simMotorRun([90,270,140,265,180], [0,1,2,3,4])
+    time.sleep(1)
+    motor.simMotorRun([200,100,180],[3,2,0])
 
 
 
