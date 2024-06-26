9d2cd617746cfe9df89e37b23c76187ccdf4e4a5
Botbuild <andrewyu1470963@gmail.com>
2024-06-26 13:37:50 -0700

    Motor Test

--- MotorControl/Integration.py
+++ MotorControl/Integration.py
@@ -200,7 +200,7 @@ if __name__ == "__main__":
     motor.simMotorRun([200,80,180],[3,2,4])
     motor.dxlSetVelo([55,55,55],[2,3,4])
     time.sleep(0.1)
-    motor.simMotorRun([60,170,150],[2,3,4])
+    motor.simMotorRun([60,160,150],[2,3,4])
     time.sleep(0.3)
     motor.simMotorRun([200,80,180],[3,2,4])    
    
