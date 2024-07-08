6675c284d3dfd56ed60863e45ec76c96127a0c7d
Winson Zhu <winson.zhu10@gmail.com>
2024-07-08 13:39:55 -0700

    TEST

--- motor_pkg/motor_pkg/rasptoarduino.py
+++ motor_pkg/motor_pkg/rasptoarduino.py
@@ -2,13 +2,12 @@
 import serial
 import time
 
-def handmotor():
+if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
-    ser.write(b"Response: \n")
-    line = ser.readline().decode('utf-8').rstrip()
-    print(line)
-    time.sleep(5)
 
-if __name__ == '__main__':
-    handmotor()
+    while True:
+        ser.write(b"Hello from Raspberry Pi!\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
