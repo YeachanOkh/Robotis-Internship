56728daf280e1e20806c41e5fb51c940b897750f
YeachanOkh <yeachanokh@gmail.com>
2024-07-10 13:07:57 -0700

    test

--- motor_pkg/motor_pkg/rasptoarduino.py
+++ motor_pkg/motor_pkg/rasptoarduino.py
@@ -2,11 +2,11 @@
 import serial
 import time
 
-def handmotor():
+def handmotor(response):
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
-    # changetobyte=response.encode(encoding="utf-8") 
-    ser.write(b"Response")
+    changetobyte=response.encode(encoding="utf-8") 
+    ser.write(b"Response" + changetobyte)
     line = ser.readline().decode('utf-8').rstrip()
     print(line)
     time.sleep(5)
