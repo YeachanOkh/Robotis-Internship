d63cb1709eb13701968c9670ccc985db7baa4a69
YeachanOkh <yeachanokh@gmail.com>
2024-07-11 17:34:24 -0700

    test

--- motor_pkg/motor_pkg/rasptoarduino.py
+++ motor_pkg/motor_pkg/rasptoarduino.py
@@ -4,12 +4,12 @@ import time
 
 def handmotor(response):
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    changetobyte=response.encode(encoding="utf-8") 
+    ser.write(changetobyte)
     while True:
-        ser.reset_input_buffer()
-        changetobyte=response.encode(encoding="utf-8") 
-        ser.write(changetobyte)
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
+        ser.reset_input_buffer()
 
 if __name__ == '__main__':
     handmotor()
--- robotis-Internship/MotorControl/arduinocode.cpp
+++ robotis-Internship/MotorControl/arduinocode.cpp
@@ -1,17 +1,19 @@
 #include <Servo.h>
 
+// Servo objects
 Servo Servo1;
 Servo Servo2;
 Servo Servo3;
 Servo Servo4;
 Servo Servo5;
 
-const int motor1 = 3; //thumb
-const int motor2 = 5; //index
-const int motor3 = 6; //middle
-const int motor4 = 9; //ring finger
-const int motor5 = 10; //pinky
-int motorposition[5] = {0,0,0,0,0}; // M1, M2, M3, M4, M5
+// Motor pin assignments
+const int motor1 = 3; // thumb
+const int motor2 = 5; // index
+const int motor3 = 6; // middle
+const int motor4 = 9; // ring finger
+const int motor5 = 10; // pinky
+int motorposition[5] = {0, 0, 0, 0, 0}; // M1, M2, M3, M4, M5
 
 void setup() {
   Servo1.attach(motor1);
@@ -22,176 +24,180 @@ void setup() {
   Serial.begin(9600);
 }
 
-void closehand(){ //fistbump, yes
-    for (int i=1; i<=60; i++){
-      if (motorposition[0] > 0) {
-        motorposition[0]--;
-        Servo1.write(motorposition[0]);
-      }
-      if (motorposition[1] > 0) {
-      motorposition[1]--;
-      Servo2.write(motorposition[1]);
-      }
-      if (motorposition[2] > 0) {
-      motorposition[2]--;
-      Servo3.write(motorposition[2]);
-      }
-      if (motorposition[3] > 0) {
-      motorposition[3]--;
-      Servo4.write(motorposition[3]);
-      }
-      if (motorposition[4] > 0) {
-      motorposition[4]--;
-      Servo5.write(motorposition[4]);
-      }
-      if (motorposition[0] == 0 && motorposition[1] == 0 && motorposition[2] == 0 && 
-      motorposition[3] == 0 && motorposition[4] == 0) {
-        break;
-        }
-      delay(20);
-    }
-  }
-
-void openhand() { //high five, thank you
-    for (int i=1; i<=60; i++){
-      if (motorposition[0] < 60) {
-        motorposition[0]++;
-        Servo1.write(motorposition[0]);
-      }
-      if (motorposition[1] < 60) {
-        motorposition[1]++;
-        Servo2.write(motorposition[1]);
-      }
-      if (motorposition[2] < 60) {
-        motorposition[2]++;
-        Servo3.write(motorposition[2]);
-      }
-      if (motorposition[3] < 60) {
-        motorposition[3]++;
-        Servo4.write(motorposition[3]);
-      }
-      if (motorposition[4] < 60) {
-        motorposition[4]++;
-        Servo5.write(motorposition[4]);
-      }
-      if (motorposition[0] == 60 && motorposition[1] == 60 && motorposition[2] == 60 && 
-      motorposition[3] == 60 && motorposition[4] == 60) {
-        break;
-        }
-      delay(20);
-    }
-  }
-
-void hello() { //starting open hand
-  openhand();
-  for (int i=1; i<=60; i++){ // thumb moving 60
+void closehand() { // fistbump, yes
+  for (int i = 1; i <= 60; i++) {
+    if (motorposition[0] > 0) {
       motorposition[0]--;
       Servo1.write(motorposition[0]);
-      delay(20);
     }
-  }
-
-void handshake() { //starting open hand
-  openhand();
-  for (int i=1; i<=60; i++){
-      if (i % 3 == 0) { //thumb moving 20
-        motorposition[0]--;
-        Servo1.write(motorposition[0]);
-      }
-      if (i % 6 == 0) { //index moving 10
-        motorposition[1]--;
-        Servo2.write(motorposition[1]);
-      }
-      if (i % 6 == 0) { //middle moving 10
-        motorposition[2]--;
-        Servo3.write(motorposition[2]);
-      }
-      if (i % 4 == 0) { //ring finger moving 15
-        motorposition[3]--;
-        Servo4.write(motorposition[3]);
-      }
-      if (i % 3 == 0) { // pinky moving 20
-        motorposition[4]--;
-        Servo5.write(motorposition[4]);
-      }
-      delay(7);
+    if (motorposition[1] > 0) {
+      motorposition[1]--;
+      Servo2.write(motorposition[1]);
     }
-  }
-
-void no () {
-    openhand();
-    for (int i=1; i<=60; i++){
-      if (i % 2 == 0) { //thumb moving 30
-        motorposition[0]--;
-        Servo1.write(motorposition[0]);
-      }
-      if (i % 3 == 0) { //index moving 20
-        motorposition[1]--;
-        Servo2.write(motorposition[1]);
-      }
-      if (i % 3 == 0) { //middle moving 20
-        motorposition[2]--;
-        Servo3.write(motorposition[2]);
-      } 
-      //ring finger moving 60
+    if (motorposition[2] > 0) {
+      motorposition[2]--;
+      Servo3.write(motorposition[2]);
+    }
+    if (motorposition[3] > 0) {
       motorposition[3]--;
       Servo4.write(motorposition[3]);
-      // pinky moving 60
+    }
+    if (motorposition[4] > 0) {
+      motorposition[4]--;
+      Servo5.write(motorposition[4]);
+    }
+    if (motorposition[0] == 0 && motorposition[1] == 0 && motorposition[2] == 0 &&
+        motorposition[3] == 0 && motorposition[4] == 0) {
+      break;
+    }
+    delay(20);
+  }
+}
+
+void openhand() { // high five, thank you
+  for (int i = 1; i <= 60; i++) {
+    if (motorposition[0] < 60) {
+      motorposition[0]++;
+      Servo1.write(motorposition[0]);
+    }
+    if (motorposition[1] < 60) {
+      motorposition[1]++;
+      Servo2.write(motorposition[1]);
+    }
+    if (motorposition[2] < 60) {
+      motorposition[2]++;
+      Servo3.write(motorposition[2]);
+    }
+    if (motorposition[3] < 60) {
+      motorposition[3]++;
+      Servo4.write(motorposition[3]);
+    }
+    if (motorposition[4] < 60) {
+      motorposition[4]++;
+      Servo5.write(motorposition[4]);
+    }
+    if (motorposition[0] == 60 && motorposition[1] == 60 && motorposition[2] == 60 &&
+        motorposition[3] == 60 && motorposition[4] == 60) {
+      break;
+    }
+    delay(20);
+  }
+}
+
+void hello() { // starting open hand
+  openhand();
+  for (int i = 1; i <= 60; i++) { // thumb moving 60
+    motorposition[0]--;
+    Servo1.write(motorposition[0]);
+    delay(20);
+  }
+}
+
+void handshake() { // starting open hand
+  openhand();
+  for (int i = 1; i <= 60; i++) {
+    if (i % 3 == 0) { // thumb moving 20
+      motorposition[0]--;
+      Servo1.write(motorposition[0]);
+    }
+    if (i % 6 == 0) { // index moving 10
+      motorposition[1]--;
+      Servo2.write(motorposition[1]);
+    }
+    if (i % 6 == 0) { // middle moving 10
+      motorposition[2]--;
+      Servo3.write(motorposition[2]);
+    }
+    if (i % 4 == 0) { // ring finger moving 15
+      motorposition[3]--;
+      Servo4.write(motorposition[3]);
+    }
+    if (i % 3 == 0) { // pinky moving 20
+      motorposition[4]--;
+      Servo5.write(motorposition[4]);
+    }
+    delay(7);
+  }
+}
+
+void no() {
+  openhand();
+  for (int i = 1; i <= 60; i++) {
+    if (i % 2 == 0) { // thumb moving 30
+      motorposition[0]--;
+      Servo1.write(motorposition[0]);
+    }
+    if (i % 3 == 0) { // index moving 20
+      motorposition[1]--;
+      Servo2.write(motorposition[1]);
+    }
+    if (i % 3 == 0) { // middle moving 20
+      motorposition[2]--;
+      Servo3.write(motorposition[2]);
+    }
+    // ring finger moving 60
+    motorposition[3]--;
+    Servo4.write(motorposition[3]);
+    // pinky moving 60
+    motorposition[4]--;
+    Servo5.write(motorposition[4]);
+    delay(20);
+  }
+}
+
+void goodbye() { // starting openhand
+  for (int i = 1; i <= 3; i++) {
+    openhand();
+    for (int i = 1; i <= 60; i++) {
+      motorposition[1]--;
+      Servo2.write(motorposition[1]);
+      motorposition[2]--;
+      Servo3.write(motorposition[2]);
+      motorposition[3]--;
+      Servo4.write(motorposition[3]);
       motorposition[4]--;
       Servo5.write(motorposition[4]);
       delay(20);
     }
   }
+  openhand();
+}
 
-void goodbye () { // starting openhand
-    for (int i=1; i<=3; i++) {
-      openhand();
-      for (int i=1; i<=60; i++) {
-        motorposition[1]--;
-        Servo2.write(motorposition[1]);
-        motorposition[2]--;
-        Servo3.write(motorposition[2]);
-        motorposition[3]--;
-        Servo4.write(motorposition[3]);
-        motorposition[4]--;
-        Servo5.write(motorposition[4]);
-        delay(20);
-      }
-    }
+void switchstatement(String data) {
+  data.trim(); // Remove any leading or trailing whitespace
+  if (data == "hello") {
+    Serial.println("hello activated");
+    hello();
+  } else if (data == "no") {
+    Serial.println("no activated");
+    no();
+  } else if (data == "thankyou") {
+    Serial.println("thankyou activated");
     openhand();
+  } else if (data == "highfive") {
+    Serial.println("highfive activated");
+    openhand();
+  } else if (data == "handshake") {
+    Serial.println("handshake activated");
+    handshake();
+  } else if (data == "goodbye") {
+    Serial.println("goodbye activated");
+    goodbye();
+  } else if (data == "yes") {
+    Serial.println("yes activated");
+    closehand();
+  } else if (data == "fistbump") {
+    Serial.println("fistbump activated");
+    closehand();
+  } else {
+    Serial.println("did not execute");
   }
-// Dictionary mapping commands to functions
-std::unordered_map<String, std::function<void()>> Command_dict = {
-    {"hello", hello},
-    {"no", no},
-    {"thankyou", openhand},
-    {"handshake", handshake},
-    {"highfive", openhand},
-    {"goodbye", goodbye},
-    {"yes", closehand},
-    {"fistbump", closehand}
-};
+}
 
 void loop() {
   if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    if (Command_dict.find(data) != Command_dict.end()) {
-      Serial.print("You sent me: "+data);
-      Command_dict[data]();
-    } else {
-      Serial.println("Command not found");
-    }
-    // for (int i=0; i<55; i++){
-    //   motorposition[0]++;
-      
-    //   Servo1.write(motorposition[0]);
-      
-    //   delay(20);
-    // }
-    // for (int i=0; i<55; i++){
-    //   motorposition[0]--;
-    //   Servo1.write(motorposition[0]);
-    //   delay(20);
-    // }
+    String data = Serial.readString();
+    Serial.println("You sent me: " + data);
+    switchstatement(data);
   }
 }
