49af1adaef7eff5440864dd14f25230fc0c6ae0e
Botbuild <andrewyu1470963@gmail.com>
2024-07-15 12:13:16 -0700

    Motor

--- motor_pkg/motor_pkg/Integration.py
+++ motor_pkg/motor_pkg/Integration.py
@@ -157,7 +157,7 @@ def Goodbye():
 def startsetup():
     print("set up move")
     motor.dxlSetVelo([30, 30, 30, 30, 30], [0, 1, 2, 3, 4])
-    motor.simMotorRun([90, 270, 135, 265, 267], [0, 1, 2, 3, 4])
+    motor.simMotorRun([90, 270, 133, 265, 267], [0, 1, 2, 3, 4])
     time.sleep(3)
 
 # Dictionary mapping commands to functions
