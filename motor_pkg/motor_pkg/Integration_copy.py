164cdb827caa2ca055435377beb3e14d35e0f039
Botbuild <andrewyu1470963@gmail.com>
2024-08-01 10:30:05 -0700

    test

--- motor_pkg/motor_pkg/Integration_copy.py
+++ motor_pkg/motor_pkg/Integration_copy.py
@@ -189,8 +189,9 @@ def Goodbye():
 def startsetup():
     print("set up move")
     motor.dxlSetVelo([30, 30, 30, 30, 30], [0, 1, 2, 3, 4])
-    motor.simMotorRun([90, 270, 140, 265, 270], [0, 1, 2, 3, 4])
+    motor.simMotorRun([90, 270, 130, 264, 270], [0, 1, 2, 3, 4])
     time.sleep(1)
+    hand.handmotor("openhand")
 
 # Dictionary mapping commands to functions
 Command_dict = {
